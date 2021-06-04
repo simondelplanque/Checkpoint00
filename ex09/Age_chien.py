eaf5ee0116c8ad47507cb9c0bb4fce5ee63eb3b4
KnElisabeth <elisabeth.hoarau@kndigit>
2021-06-04 11:53:32 +0200

    exo 09 ok

---
+++ ex09/Age_chien.py
@@ -0,0 +1,10 @@
+def agechien(age_ch):
+
+        ageterre = age_ch*7
+        print (f"Votre chien a {ageterre} ans.")
+
+
+
+age = int(input ("Combien d'année à votre chien ?\n"))
+
+agechien(age)
